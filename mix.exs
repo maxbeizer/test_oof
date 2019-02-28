77e479b2df67a7ed3471ff2d66b56ffdd2d5a836
maxbeizer <max.beizer@gmail.com>
2019-02-27 22:11:16 -0600

    Bump to Elixir 1.7, get ready to publish

--- mix.exs
+++ mix.exs
@@ -5,24 +5,30 @@ defmodule TestOof.MixProject do
     [
       app: :test_oof,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Never miss the compiler warning for _test.ex files again.",
+      package: [
+        name: "test_oof",
+        files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/maxbeizer/test_oof"}
+      ],
+      source_url: "https://github.com/maxbeizer/test_oof",
+      homepage_url: "https://github.com/maxbeizer/test_oof"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
